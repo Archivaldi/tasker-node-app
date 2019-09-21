dc9fd8fd9f08a9211fd563dbf63ef036b904b977
Artur Markov <artur.markov1860@gmail.com>
2019-09-21 13:28:01 -0700

    nodemailer deleted cuz of glitches

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 exports.data = {
     user: process.env.USER,
-    pass: process.env.PASSWORD
+    pass: process.env.PASSWORD,
+    type: process.env.TYPE
 };
 
 exports.service = process.env.SERVICE;
