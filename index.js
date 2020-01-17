78c203d9e3964c2daa9a1e38c227fc739ee1be00
Artur Markov <artur.markov1860@gmail.com>
2020-01-17 12:38:18 -0800

    taking body, html and attachments

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-const Nightmare = require("nightmare");
-const nightmare = Nightmare();
-var nodemailer = require('nodemailer');
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+const nodemailer = require('nodemailer');
 require("dotenv").config();
 const keys = require("./keys.js")
+const fs = require('fs');
 
-var transporter = nodemailer.createTransport({
-    service: keys.service,
-    auth: keys.data
-  });
+const transporter = nodemailer.createTransport({
+  service: keys.service,
+  auth: keys.data
+});
 
 
-nightmare.goto("https://sites.google.com/taskrabbit.com/northamerica/available-ikea-tasks-for-pst#h.p_MZ5hNI0XhhTv")
-            .wait(5000)
-            .evaluate(function(){
-                var jobs = document.getElementById("h.p_7hl2UO4_8Kwx");
-                return jobs.textContent;
-            })
-            .end()
-            .then(result => {
-                console.log(result);
 
-                var mailOptions = {
-                    from: 'artur.markov1860@gmail.com',
-                    to: "archivaldi95@yandex.ru",
-                    subject: 'Sending Email using Node.js',
-                    text: result
-                  };
+nightmare.goto("https://sites.google.com/taskrabbit.com/northamerica/available-ikea-tasks-for-pst")
+  .wait(5000)
+  .evaluate(function () {
+    var jobs = document.getElementById("h.p_jNme5yJ2hWl8");
+    return jobs.innerHTML;
+  })
+  .end()
+  .then(result => {
 
-                  transporter.sendMail(mailOptions, function(error, info){
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log('Email sent: ' + info.response);
-                    }
-                  });
-            })
+    fs.writeFile('jobs.html', result, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+
+    var mailOptions = {
+      from: 'artur.markov1860@gmail.com',
+      to: "archivaldi95@yandex.ru",
+      subject: 'Sending Email using Node.js',
+      attachments: [
+        { 
+            filename: 'jobs.html',
+            content: result
+        }],
+        text: "There are new Jobs!"
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }); 
